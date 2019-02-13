5d461f421ae6a08e19e1ff231935aa9be1157629
Matt Driscoll <MDriscoll@esri.com>
2019-02-13 10:46:49 -0800

    checkin things

---
+++ demos/custom-compass/app/CustomCompass.js
@@ -0,0 +1,115 @@
+/// <amd-dependency path="esri/core/tsSupport/declareExtendsHelper" name="__extends" />
+/// <amd-dependency path="esri/core/tsSupport/decorateHelper" name="__decorate" />
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+define(["require", "exports", "esri/core/tsSupport/declareExtendsHelper", "esri/core/tsSupport/decorateHelper", "esri/widgets/Widget", "esri/widgets/Compass/CompassViewModel", "esri/core/accessorSupport/decorators", "esri/widgets/support/widget"], function (require, exports, __extends, __decorate, Widget, CompassViewModel, decorators_1, widget_1) {
+    "use strict";
+    var CSS = {
+        base: "custom-compass",
+        disabled: "custom-compass--disabled",
+        image: "custom-compass__image",
+        // common
+        esriWidget: "esri-widget",
+        esriHeader: "esri-widget__header",
+        esriIconPlay: "esri-icon-play",
+        esriIconPause: "esri-icon-pause"
+    };
+    var CustomCompass = /** @class */ (function (_super) {
+        __extends(CustomCompass, _super);
+        //--------------------------------------------------------------------------
+        //
+        //  Lifecycle
+        //
+        //--------------------------------------------------------------------------
+        function CustomCompass(props) {
+            var _this = _super.call(this) || this;
+            //--------------------------------------------------------------------------
+            //
+            //  Variables
+            //
+            //--------------------------------------------------------------------------
+            //--------------------------------------------------------------------------
+            //
+            //  Properties
+            //
+            //--------------------------------------------------------------------------
+            //----------------------------------
+            //  view
+            //----------------------------------
+            _this.view = null;
+            //----------------------------------
+            //  viewModel
+            //----------------------------------
+            _this.viewModel = new CompassViewModel();
+            return _this;
+        }
+        CustomCompass.prototype.postInitialize = function () { };
+        //--------------------------------------------------------------------------
+        //
+        //  Public Methods
+        //
+        //--------------------------------------------------------------------------
+        CustomCompass.prototype.reset = function () { };
+        CustomCompass.prototype.render = function () {
+            var _a;
+            var _b = this.viewModel, orientation = _b.orientation, state = _b.state;
+            var disabled = state === "disabled"; // todo: cracked glass?
+            var baseClasses = (_a = {},
+                _a[CSS.disabled] = disabled,
+                _a);
+            var compassImage = disabled ? null : (widget_1.tsx("img", { class: CSS.image, src: "app/img/compass-needle.png", alt: "Compass Needle", styles: this._toRotationTransform(orientation) }));
+            return (widget_1.tsx("button", { bind: this, class: this.classes(CSS.base, baseClasses), onclick: this._reset, "aria-label": "Reset", title: "Reset" }, compassImage));
+        };
+        //--------------------------------------------------------------------------
+        //
+        //  Protected Methods
+        //
+        //--------------------------------------------------------------------------
+        //--------------------------------------------------------------------------
+        //
+        //  Private Methods
+        //
+        //--------------------------------------------------------------------------
+        CustomCompass.prototype._reset = function () {
+            this.viewModel.reset();
+        };
+        CustomCompass.prototype._toRotationTransform = function (orientation) {
+            return {
+                transform: "rotateZ(" + orientation.z + "deg)"
+            };
+        };
+        __decorate([
+            decorators_1.aliasOf("viewModel.view")
+        ], CustomCompass.prototype, "view", void 0);
+        __decorate([
+            decorators_1.property(),
+            widget_1.renderable(["viewModel.orientation", "viewModel.state"])
+        ], CustomCompass.prototype, "viewModel", void 0);
+        __decorate([
+            decorators_1.aliasOf("viewModel.reset")
+        ], CustomCompass.prototype, "reset", null);
+        CustomCompass = __decorate([
+            decorators_1.subclass("esri.demo.CustomCompass")
+        ], CustomCompass);
+        return CustomCompass;
+    }(decorators_1.declared(Widget)));
+    return CustomCompass;
+});
+//# sourceMappingURL=CustomCompass.js.map
---
+++ demos/custom-compass/app/CustomCompass.tsx
@@ -0,0 +1,137 @@
+/// <amd-dependency path="esri/core/tsSupport/declareExtendsHelper" name="__extends" />
+/// <amd-dependency path="esri/core/tsSupport/decorateHelper" name="__decorate" />
+
+import MapView = require("esri/views/MapView");
+import Widget = require("esri/widgets/Widget");
+
+import CompassViewModel = require("esri/widgets/Compass/CompassViewModel");
+
+import { aliasOf, declared, property, subclass } from "esri/core/accessorSupport/decorators";
+import { renderable, tsx } from "esri/widgets/support/widget";
+
+const CSS = {
+  base: "custom-compass",
+  disabled: "custom-compass--disabled",
+  image: "custom-compass__image",
+
+  // common
+  esriWidget: "esri-widget",
+  esriHeader: "esri-widget__header",
+  esriIconPlay: "esri-icon-play",
+  esriIconPause: "esri-icon-pause"
+};
+
+interface CustomCompassProperties {
+  view: MapView;
+}
+
+interface Axes {
+  x?: number;
+  y?: number;
+  z?: number;
+}
+
+@subclass("esri.demo.CustomCompass")
+class CustomCompass extends declared(Widget) {
+  //--------------------------------------------------------------------------
+  //
+  //  Lifecycle
+  //
+  //--------------------------------------------------------------------------
+
+  constructor(props: CustomCompassProperties) {
+    super();
+  }
+
+  postInitialize() {}
+
+  //--------------------------------------------------------------------------
+  //
+  //  Variables
+  //
+  //--------------------------------------------------------------------------
+
+  //--------------------------------------------------------------------------
+  //
+  //  Properties
+  //
+  //--------------------------------------------------------------------------
+
+  //----------------------------------
+  //  view
+  //----------------------------------
+
+  @aliasOf("viewModel.view") view: MapView = null;
+
+  //----------------------------------
+  //  viewModel
+  //----------------------------------
+
+  @property()
+  @renderable(["viewModel.orientation", "viewModel.state"])
+  viewModel: CompassViewModel = new CompassViewModel();
+
+  //--------------------------------------------------------------------------
+  //
+  //  Public Methods
+  //
+  //--------------------------------------------------------------------------
+
+  @aliasOf("viewModel.reset")
+  reset(): void {}
+
+  render() {
+    const { orientation, state } = this.viewModel;
+
+    const disabled = state === "disabled"; // todo: cracked glass?
+
+    const baseClasses = {
+      [CSS.disabled]: disabled
+    };
+
+    const compassImage = disabled ? null : (
+      <img
+        class={CSS.image}
+        src="app/img/compass-needle.png"
+        alt="Compass Needle"
+        styles={this._toRotationTransform(orientation)}
+      />
+    );
+
+    return (
+      <button
+        bind={this}
+        class={this.classes(CSS.base, baseClasses)}
+        onclick={this._reset}
+        aria-label="Reset"
+        title="Reset"
+      >
+        {compassImage}
+      </button>
+    );
+  }
+
+  //--------------------------------------------------------------------------
+  //
+  //  Protected Methods
+  //
+  //--------------------------------------------------------------------------
+
+  //--------------------------------------------------------------------------
+  //
+  //  Private Methods
+  //
+  //--------------------------------------------------------------------------
+
+  private _reset(): void {
+    this.viewModel.reset();
+  }
+
+  private _toRotationTransform(orientation: Axes): HashMap<string> {
+    return {
+      transform: `rotateZ(${orientation.z}deg)`
+    };
+  }
+}
+
+export = CustomCompass;
